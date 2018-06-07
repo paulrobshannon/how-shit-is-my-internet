59b26ab8081d3e7776b6b08a82f429be553e80d2
Paul Shannon <Paul.Shannon@allstate.com>
2018-06-07 11:41:20 +0100

    adding stuff

--- code/1-check_connections.py
+++ code/1-check_connections.py
@@ -13,19 +13,27 @@ def is_connected():
         pass
     return False
 
-i = 0
+outage_time = None
+outage_end  = None
 
-while i < 10:
-    df = pd.DataFrame({'time': pd.datetime.now(),
-                   'connected': [is_connected()]})
-    
-    if not os.path.isfile('./data/connection_info.csv'):
-        df.to_csv('./data/connection_info.csv', index=False)
+while True:
+    if not is_connected():
+        outage_time = pd.datetime.now() if outage_time is None else outage_time
+
     else:
-        df.to_csv('./data/connection_info.csv', mode='a', header=False, index=False)
+        if outage_time is not None:
+                outage_end = pd.datetime.now()
+
+                df = pd.DataFrame({'outage_time': [outage_time],
+                                   'outage_end' : [outage_end]})
+
+                if not os.path.isfile('../data/connection_info.csv'):
+                    df.to_csv('../data/connection_info.csv', index=False)
+
+                else:
+                    df.to_csv('../data/connection_info.csv', mode='a', header=False, index=False)
+
+                outage_time = None
+                outage_end  = None
         
     time.sleep(1)
-    
-    i += 1
-
-# os.remove('./data/connection_info.csv')
